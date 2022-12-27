bc8e7bc1a04c9224452d6bc5ee489347d38a3b27
Nick Doornekamp <nddoornekamp@gmail.com>
2022-12-27 19:56:37 +0100

    Replace Chromium with Chrome; load terminal config

--- setup/customize-desktop.sh
+++ setup/customize-desktop.sh
@@ -2,4 +2,4 @@
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'code_code.desktop', 'chromium_chromium.desktop', 'org.gnome.Terminal.desktop']"
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'code_code.desktop', 'org.gnome.Terminal.desktop', 'google-chrome.desktop']"
--- setup/setup-ubuntu.sh
+++ setup/setup-ubuntu.sh
@@ -6,14 +6,18 @@ apt update -y && apt upgrade -y
 apt install git -y
 apt install python3 python3-venv python3-pip -y
 
-apt install chromium-browser -y
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt-get update
+apt-get install google-chrome-stable
 snap install --classic code
 
-sh customize-desktop.sh
+sh setup/customize-desktop.sh
+dconf load /org/gnome/terminal/legacy/profiles:/ < gnome/gnome-terminal-profiles.dconf
 
-sh docker-install.sh
-sh docker post-install.sh
+sh setup/docker-install.sh
+sh setup/docker-post-install.sh
 
-sh setup-zsh.sh
+apt install zsh
 
 apt update -y && apt upgrade -y
