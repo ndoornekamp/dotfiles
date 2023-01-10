279deb4055bc96a5317181aaa96e3c6a366c70cb
Nick Doornekamp <nddoornekamp@gmail.com>
2023-01-10 15:25:28 +0100

    Add conda init to miniconda-install

--- setup/miniconda-install.sh
+++ setup/miniconda-install.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
-set -e
+
+~/miniconda3/bin/conda init zsh
 
 if ! [ -x "$(command -v conda)" ]; then
     echo "Conda is not yet installed"
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash Miniconda3-latest-Linux-x86_64.sh -b
     rm Miniconda3-latest-Linux-x86_64.sh -f
-
     export PATH="~/miniconda3/bin":$PATH
-    conda init zsh
-    exit
 else
     echo "Conda is already installed"
-    exit
 fi
