041903b12b057c039ee094c8cd2438eb4d18e5a0
Nick Doornekamp <nddoornekamp@gmail.com>
2023-05-07 11:33:30 +0200

    Display time in dock

--- setup/customize-desktop.sh
+++ setup/customize-desktop.sh
@@ -3,3 +3,4 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'code_code.desktop', 'org.gnome.Terminal.desktop', 'google-chrome.desktop']"
+gsettings set org.gnome.desktop.interface clock-show-seconds true
